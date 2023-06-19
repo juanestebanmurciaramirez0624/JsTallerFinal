56d5d6414b2aa834b6a46760edb2381b69cf85b0
juanestebanmurciaramirez0624 <116554534+juanestebanmurciaramirez0624@users.noreply.github.com>
2023-06-18 21:44:10 -0500

    Update FuncionInstructor.js

--- js/FuncionInstructor.js
+++ js/FuncionInstructor.js
@@ -1,7 +1,7 @@
 let enviar = document.getElementById("enviar");
 enviar.addEventListener("click", instructor);
 
-function instructor(){
+function instructor() {
     let nameinstructor = document.getElementById("nameinstructor").value;
     let lastnameinstructor = document.getElementById("lastnameinstructor").value;
     let materia = document.getElementById("materia").value;
@@ -9,28 +9,31 @@ function instructor(){
     if (nameinstructor == "Yaneth" && lastnameinstructor == "Castillo" && materia == "Proteccion") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Manuel" && lastnameinstructor == "Garavito" && materia == "Python"){
+    else if (nameinstructor == "Manuel" && lastnameinstructor == "Garavito" && materia == "Python") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Erick" && lastnameinstructor == "Granados" && materia == "java"){
+    else if (nameinstructor == "Erick" && lastnameinstructor == "Granados" && materia == "java") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Paula" && lastnameinstructor == "Romero" && materia == "Emprendimiento"){
+    else if (nameinstructor == "Paula" && lastnameinstructor == "Romero" && materia == "Emprendimiento") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Narly" && lastnameinstructor == "Sanchez" && materia == "SQL" || materia == "js"){
+    else if (nameinstructor == "Narly" && lastnameinstructor == "Sanchez" && materia == "SQL") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Cristian" && lastnameinstructor == "Buitrago" && materia == "Proyecto"){
+    else if (nameinstructor == "Narly" && lastnameinstructor == "Sanchez" && materia == "js") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Julian" && lastnameinstructor == "Aranguren" && materia == "Investigacion"){
+    else if (nameinstructor == "Cristian" && lastnameinstructor == "Buitrago" && materia == "Proyecto") {
         alert("La respuesta es correcta");
     }
-    else if(nameinstructor == "Andrea" && lastnameinstructor == "Ramos" && materia == "Comunicacion"){
+    else if (nameinstructor == "Julian" && lastnameinstructor == "Aranguren" && materia == "Investigacion") {
         alert("La respuesta es correcta");
     }
-    else{
+    else if (nameinstructor == "Andrea" && lastnameinstructor == "Ramos" && materia == "Comunicacion") {
+        alert("La respuesta es correcta");
+    }
+    else {
         alert("Respuesta es incorrecta")
     }
 }
