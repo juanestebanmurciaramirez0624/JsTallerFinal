e18933f274a032d9e4319bf72918219725591248
juanestebanmurciaramirez0624 <116554534+juanestebanmurciaramirez0624@users.noreply.github.com>
2023-06-18 21:22:20 -0500

    Update FuncionGrados.js

--- js/FuncionGrados.js
+++ js/FuncionGrados.js
@@ -1,7 +1,7 @@
 let enviar = document.getElementById("enviar");
 enviar.addEventListener("click", grados);
 
-function grados(){
+function grados() {
     let grados = Number(document.getElementById("grados").value);
     let tipoGrados = document.getElementById("tipoGrados").value;
     let convertirGrados = document.getElementById("convertirGrados").value;
@@ -11,18 +11,18 @@ function grados(){
     }
     else if (tipoGrados == "Fahrenheit") {
         if (convertirGrados == "Celsius") {
-            resultado = (grados-32)*5/9;
+            resultado = (grados - 32) * 5 / 9;
         }
-        else if (convertirGrados == "Kelvin"){
+        else if (convertirGrados == "Kelvin") {
             resultado = (grados - 32);
-            resultado = resultado *5/9 +273.15;
+            resultado = resultado * 5 / 9 + 273.15;
         }
     }
     if (tipoGrados == "Celsius") {
         if (convertirGrados == "Fahrenheit") {
-            resultado = (grados * 5/9) + 32 ;
+            resultado = (grados * 1.8) + 32;
         }
-        else if (convertirGrados == "Kelvin"){
+        else if (convertirGrados == "Kelvin") {
             resultado = grados + 273.15;
         }
     }
@@ -30,10 +30,10 @@ function grados(){
         if (convertirGrados == "Celsius") {
             resultado = grados - 273.15;
         }
-        else if (convertirGrados == "Fahrenheit"){
-            resultado = (grados - 273.15)*9/5 + 32;
+        else if (convertirGrados == "Fahrenheit") {
+            resultado = (grados - 273.15) * 9 / 5 + 32;
         }
     }
-    alert("La convercion de "+grados+" grados "+tipoGrados+"a grados "+convertirGrados+" es: "+resultado);  
+    alert("La convercion de " + grados + " grados " + tipoGrados + " a grados " + convertirGrados + " es: " + resultado);
 }
 
